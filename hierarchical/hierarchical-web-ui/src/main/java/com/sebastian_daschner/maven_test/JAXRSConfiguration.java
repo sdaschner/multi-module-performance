f7a45e7d11c1c8fe150e71411eff09034387384a
Sebastian Daschner <git@sebastian-daschner.de>
2016-01-13 18:39:41 +0100

    removed generated code

--- hierarchical/hierarchical-web-ui/src/main/java/com/sebastian_daschner/maven_test/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.sebastian_daschner.maven_test;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-    // nothing to configure
-}
